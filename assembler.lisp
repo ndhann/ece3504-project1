a4a1bd0b782b198a867b0f519140777b7d40e3f0
Nathan D <99107654+ndhann@users.noreply.github.com>
2022-10-03 10:57:15 -0400

    Adds code to read the input file into a list

--- assembler.lisp
+++ assembler.lisp
@@ -1,10 +1,19 @@
 (require :uiop)
-(if (eq (uiop:file-exists-p (car (uiop:command-line-arguments))) nil)
+;(require :split-sequence)
+(defvar inputpathname "")
+(defvar inputlines nil)
+(if (uiop:file-exists-p (car (uiop:command-line-arguments)))
+    (setf inputpathname (pathname (car (uiop:command-line-arguments)))))
+
+(if (eq (uiop:file-exists-p inputpathname) nil)
     (progn
-      (print "provide a valid filename")
+      (print "provide a valid .s file")
       (exit :CODE 1)))
 (let ((in (open (car (uiop:command-line-arguments)) :if-does-not-exist nil)))
   (when in
     (loop for line = (read-line in nil)
-          while line do (format t "~a~%" line))
+          while line do (setf inputlines (push (uiop:split-string line :separator " ") inputlines)))
     (close in)))
+
+(setf inputlines (reverse inputlines))
+(print inputlines)
